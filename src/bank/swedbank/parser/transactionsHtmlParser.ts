eb64dc3ee54a0317e96ba54121d1d8b9bb6fe66d
Raido Kaldma <raido.kaldma@hammertime.ee>
2018-05-03 19:37:42 +0300

    Swedbank fixes. EUR only. Default payer/payee name = "Swedbank".

--- src/bank/swedbank/parser/transactionsHtmlParser.ts
+++ src/bank/swedbank/parser/transactionsHtmlParser.ts
@@ -8,13 +8,14 @@ export function extractTransactions(transactionsHtml: string): TransactionRow[] 
 
     $("li", "#account-transactions-list")
         .filter(":has(.ui-grid-a)") // Filter out rows without transaction data (first row and last row for example)
+        .filter((i, el) => $(el).find(".ui-block-b").text().trim().endsWith("EUR")) // Only include EUR currency
         .each((i, liElement) => {
             const dateAsString = $(liElement).find(".ui-block-a").text();
             const amountString = $(liElement).find(".ui-block-b").text();
 
             const date = parseDate(dateAsString);
             const amount = parseNumber(amountString);
-            const name = $(liElement).find("h3").not(".ui-block-b").text();
+            const name = $(liElement).find("h3").not(".ui-block-b").text().trim() || "Swedbank";
             const description = $(liElement).find("p.text-secondary").text();
 
             transactions.push(new TransactionRow(date, amount, name, description));
