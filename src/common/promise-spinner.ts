891b33f35e0593bd24777d9ad1f1206f96052266
Raido Kaldma <raido.kaldma@hammertime.ee>
2020-06-15 22:19:13 +0300

    Added time taken (in seconds) for each step.

--- src/common/promise-spinner.ts
+++ src/common/promise-spinner.ts
@@ -1,6 +1,9 @@
+import * as chalk from "chalk";
 import {Spinner} from "cli-spinner";
+import {performance} from "perf_hooks";
 
 export async function withSpinner<T>(dasPromise: Promise<T>, message: string): Promise<T> {
+    const startTime = performance.now();
     const spinner = new Spinner(message);
     spinner.setSpinnerString("⠄⠆⠇⠋⠙⠸⠰⠠⠰⠸⠙⠋⠇⠆");
     spinner.start();
@@ -8,11 +11,18 @@ export async function withSpinner<T>(dasPromise: Promise<T>, message: string): P
     try {
         const returnValue: T = await dasPromise;
         spinner.stop(true);
-        console.log(`✅ ${message}`);
+        logMessageWithTime(`✅ ${message}`, startTime);
         return returnValue;
     } catch (error) {
         spinner.stop(true);
-        console.error(`❌ ${message}`);
+        logMessageWithTime(`❌ ${message}`, startTime);
         throw error;
     }
+}
+
+function logMessageWithTime(message, startTime) {
+    const endTime = performance.now();
+    const timeTakenSec = (endTime - startTime) / 1000;
+    const formattedTime = chalk.yellow(`(${timeTakenSec.toFixed(1)}s)`);
+    console.log(`${message} ${formattedTime}`);
 }
